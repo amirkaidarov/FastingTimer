a9fb00172dc6c7f25a7b25603ca60aeb6afcefa1
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-01-16 12:39:59 -0500

    Initial Commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Амир Кайдаров on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
